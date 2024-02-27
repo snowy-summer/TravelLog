0ace7d1c7068c509ae837f1755a40f12b59b7138
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2024-02-27 22:14:25 +0900

    Initial Commit

---
+++ TripLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripLog
+//
+//  Created by 최승범 on 2024/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
